ed6f98f2eb34d52d1c11eb9f2e5b40bd502a171b
thegitofyt <ermuunee06@gmail.com>
2025-02-25 22:19:46 +0100

    push

---
+++ WerkzeugMobil/MVVM/Model/Tools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WerkzeugMobil.MVVM.Model
+{
+    public class Tools
+    {
+        public List <Werkzeug> ListeWerkzeuge { get; set; }
+        public Dictionary <string, Werkzeug> keyValuePairs { get; set; }
+    }
+}
